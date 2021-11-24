d9ca5fb5a6dd2e170fac859e2fd87cf7902dbb75
hatedyouth <neverxforget@mail.ru>
2021-11-24 21:55:26 +0300

    HW1_createProfileView.swift

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Тимур Турлыкин on 23.11.2021.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    
+    
+    }

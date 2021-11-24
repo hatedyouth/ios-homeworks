b4b37d4e594f1b5af0d92aead771d3dd93f2decf
hatedyouth <neverxforget@mail.ru>
2021-11-24 23:57:40 +0300

    hw1_settingscommit

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Тимур Турлыкин on 24.11.2021.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+}

d84291000a406ad767f8184f32c3752b6d7dc48b
hatedyouth <neverxforget@mail.ru>
2021-12-25 14:27:57 +0300

    hw2.1_dorabotka2

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+
+class FeedViewController: UIViewController {
+    
+    struct Post {
+        var title:String
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+        navigationController?.navigationBar.backgroundColor = .lightGray
+        
+        let stackView : UIStackView = {
+        let stackView = UIStackView ()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+        }()
+        
+        
+        view.addSubview(stackView)
+        
+        let blueButton : UIButton = {
+        let blueButton = UIButton()
+        blueButton.backgroundColor = .blue
+        blueButton.setTitle("Пост", for: .normal)
+        blueButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        blueButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        blueButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return blueButton
+        }()
+        
+        let greenButton : UIButton = {
+        let greenButton = UIButton()
+        greenButton.backgroundColor = .green
+        greenButton.setTitle("Пост", for: .normal)
+        greenButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        greenButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        greenButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return greenButton
+        }()
+        
+        stackView.addArrangedSubview(blueButton)
+        stackView.addArrangedSubview(greenButton)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 130),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 240)
+        ])
+        
+        view.backgroundColor = .systemGray5
+    }
+    @objc func buttonAction() {
+        let postviewcontroller = PostViewController()
+        let post: Post = Post(title: "Post")
+        postviewcontroller.title = post.title
+        navigationController?.pushViewController(postviewcontroller, animated: true)
+    }
+}
---
+++ Navigation/InfoViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .systemPurple
+        super.viewDidLoad()
+        
+        
+        let button : UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: self.view.frame.size.width - 230, y: 400, width: 50, height: 50)
+        button.backgroundColor = UIColor.systemPink
+        button.setTitle("Alert", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return button
+        }()
+        self.view.addSubview(button)
+    }
+    @objc func buttonAction(sender: UIButton!) {
+        let alertcontroller = UIAlertController(title: "Alert", message: "Alert", preferredStyle: .alert )
+        let action1 = UIAlertAction(title: "Ok", style: .default) { (action) in
+            print("Message")
+        }
+        let action2 = UIAlertAction(title: "Cancel", style: .default) { (action) in
+            print("Message")
+        }
+        alertcontroller.addAction(action1)
+        alertcontroller.addAction(action2)
+        self.present(alertcontroller, animated: true, completion: nil)
+    }
+}

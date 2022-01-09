23ed217fad27cf09ba36a5102bc87c2029aa8010
hatedyouth <neverxforget@mail.ru>
2022-01-09 14:54:01 +0300

    blanc commit

--- Navigation/Extensions.swift
+++ Navigation/Extensions.swift
@@ -13,3 +13,14 @@ func addSubviews(_ subviews: UIView...) {
     subviews.forEach { addSubview($0) }
 }
 }
+
+
+public extension UIStackView {
+    func addArrangedSubviews(_ subviews: UIView...) {
+        subviews.forEach { addArrangedSubview($0) }
+        //        for subview in subviews {
+//            self.addArrangedSubview(subview)
+//        }
+    }
+    
+}
